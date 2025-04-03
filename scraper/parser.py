65a808653131831b6c3dc93c29c6e01bd6e0834c
lonorox <54672992+lonorox@users.noreply.github.com>
2025-04-03 20:50:20 +0400

    applied cleaners to scrapped information

--- scraper/parser.py
+++ scraper/parser.py
@@ -2,13 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 from collector import Collector
 from utils import analyzer as cl
-
-
+from models import data_models as dm
+from datetime import datetime
 class AuthorParser:
 
     def __init__(self):
         self.col = Collector()
-        self.cleaners = cl.Cleaners()
+        self.cleaners = cl.Cleaners
+        # self.data =
     def get_soup(self,url):
         try:
             response = self.col.get_method(url)
@@ -19,9 +20,18 @@ class AuthorParser:
     def get_author_info(self,url):
         soup = self.get_soup(url)
         getName = soup.select_one(".author-details > .author-title").get_text()
+        getName = self.cleaners.cleaner(getName)
         getDescription = soup.select_one(".author-details > .author-description").get_text()
+        getDescription = self.cleaners.cleaner(getDescription)
         getBirthDate = soup.select_one(".author-details > p >.author-born-date").get_text()
         getBirthPlace = soup.select_one(".author-details > p > .author-born-location").get_text()
-        return {"name":getName,"birthDate" : getBirthDate, "birthPlace" : getBirthPlace, "description":getDescription}
+        getBirthPlace = self.cleaners.cleaner(getBirthPlace)
+        getBirthDate = datetime.strptime(getBirthDate, "%B %d, %Y")
+        data = dm.Author(getName,getBirthDate,getBirthPlace,getDescription,[])
+        return data
 
 
+if __name__ == "__main__":
+    a = AuthorParser()
+    data = a.get_author_info("http://quotes.toscrape.com/author/Eleanor-Roosevelt/")
+    print(data)
