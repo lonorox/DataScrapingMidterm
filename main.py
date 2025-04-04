3641047efade3ece38b797c8d42f0321f29f817f
EleneBaiashvili <105130634+EleneBaiashvili@users.noreply.github.com>
2025-04-04 13:17:57 +0400

    added file loader and method that transforms data into two separate csv files

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from scraper import parser as pr
+from utils import file_handler as fl
 
 if __name__ == "__main__":
-    pass
+    url = 'https://quotes.toscrape.com'
+    parser = pr.MainParser()
+    data = parser.run(url, 3)
+    fileHandler = fl.SaveData()
+    fileHandler.save_as_csv(data)
+    fileHandler.save_as_json(data)
+    print(fileHandler.load_file("data/data.csv",filetype='csv'))
+    fileHandler.process_csv("data/data.csv")
